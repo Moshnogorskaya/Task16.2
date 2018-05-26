788d96f261895cd3f32acaaba537c0abac5fdd3f
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-26 20:04:15 +0300

    swipe is ready

--- src/swipe.js
+++ src/swipe.js
@@ -1,48 +1,34 @@
 import $ from 'jquery';
 import { createStore } from 'redux';
 
-const selector = '.suggestion-1';
-const profile = $(selector);
-const arrow = $(`${selector} .person__options-arrow`);
-// const deleteButton = $(`${selector} .person__delete`);
+function swipeOptions(selector) {
+  const profile = $(selector);
+  const arrow = $(`${selector} .person__options-arrow`);
+  const deleteButton = $(`${selector} .person__delete`);
 
-function options(state, action) {
-  switch (action.type) {
-    case 'SHOW':
-      return '-28.912466843%';
-    case 'HIDE':
-      return '0';
-    default:
-      return '0';
+  function options(state, action) {
+    switch (action.type) {
+      case 'SHOW':
+        return '-28.912466843%';
+      case 'HIDE':
+        return '0';
+      default:
+        return '0';
+    }
   }
+
+  const store = createStore(options);
+
+  store.subscribe(() => {
+    profile.css('margin-left', store.getState());
+  });
+
+  arrow.mouseenter(() => store.dispatch({ type: 'SHOW' }));
+  arrow.mouseleave(() => store.dispatch({ type: 'HIDE' }));
+  deleteButton.mouseenter(() => store.dispatch({ type: 'SHOW' }));
+  deleteButton.mouseleave(() => store.dispatch({ type: 'HIDE' }));
 }
 
-const store = createStore(options);
-
-store.subscribe(() => {
-  profile.css('margin-left', store.getState());
-});
-
-arrow.click(() => store.dispatch({ type: 'SHOW' }));
-// arrow.addEventListener('mouseleave', store.dispatch({ type: 'HIDE' }));
-//   const profile = $(selector);
-//   const arrow = $(`${selector} .person__options-arrow`);
-//   const deleteButton = $(`${selector} .person__delete`);
-
-//   const showOptions$ = Observable.fromEvent(arrow, 'mouseenter');
-//   const navigateOptions$ = Observable.fromEvent(deleteButton, 'mouseenter');
-//   const hideOptionsWhenLeaveDelete$ = Observable.fromEvent(deleteButton, 'mouseleave');
-//   const hideOptionsWhenLeaveArrow$ = Observable.fromEvent(arrow, 'mouseleave');
-
-//   const showOptions = () => profile.css('margin-left', '-28.912466843%');
-//   const hideOptions = () => profile.css('margin-left', '0');
-
-//   showOptions$.subscribe(() => showOptions());
-//   navigateOptions$.subscribe(() => showOptions());
-//   hideOptionsWhenLeaveDelete$.subscribe(() => hideOptions());
-//   hideOptionsWhenLeaveArrow$.subscribe(() => hideOptions());
-// }
-
-// options('.suggestion-1');
-// options('.suggestion-2');
-// options('.suggestion-3');
+swipeOptions('.suggestion-1');
+swipeOptions('.suggestion-2');
+swipeOptions('.suggestion-3');
