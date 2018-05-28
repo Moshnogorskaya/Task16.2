3912917b503e1a5323ee1902acc9c0569bf01f69
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-28 11:55:41 +0300

    in progress

--- src/profiles.js
+++ src/profiles.js
@@ -6,30 +6,38 @@ const refreshButton = $('.widget__refresh');
 
 function getProfilesList() {
   const randomOffset = Math.floor(Math.random() * 500);
-  return $.getJSON(`https://api.github.com/users?since=${randomOffset}`)
-    .done(data => data);
+  const request = $.getJSON(`https://api.github.com/users?since=${randomOffset}`);
+  // console.log(request);
+  return request;
 }
 
 function getProfile(list) {
   const person = list[Math.floor(Math.random() * list.length)];
-  return $.getJSON(`https://api.github.com/users/${person.login}`)
-    .done(data => data);
+  const request = $.getJSON(`https://api.github.com/users/${person.login}`);
+  // console.log(request);
+  return request;
 }
 
-function getUsers() {
-  const users = [];
-  const profiles = getProfilesList();
+const users = [];
+const profiles = getProfilesList();
+// console.log(profiles);
+const getUse = profiles.done((data) => {
   for (let i = 0; i < 3; i += 1) {
-    const profile = getProfile(profiles);
-    users.push(profile);
+    const profile = getProfile(data);
+    profile.done((data1) => {
+      users.push(data1);
+      // console.log(users);
+      return users;
+    });
   }
   return users;
-}
+}).done(() => users);
 
 function generateProfiles(state, action) {
   switch (action.type) {
     case 'REFRESH':
-      return getUsers();
+      console.log(getUse);
+      return getUse;
     default:
       return state;
   }
