50623df1890ad8e07d341e2871ef206e03582352
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-28 10:48:03 +0300

    splitted functions on promises

--- src/profiles.js
+++ src/profiles.js
@@ -4,15 +4,25 @@ import { createStore } from 'redux';
 const personList = $('.person');
 const refreshButton = $('.widget__refresh');
 
+function getProfilesList() {
+  const randomOffset = Math.floor(Math.random() * 500);
+  return $.getJSON(`https://api.github.com/users?since=${randomOffset}`)
+    .done(data => data);
+}
+
+function getProfile(list) {
+  const person = list[Math.floor(Math.random() * list.length)];
+  return $.getJSON(`https://api.github.com/users/${person.login}`)
+    .done(data => data);
+}
+
 function getUsers() {
   const users = [];
-  const randomOffset = Math.floor(Math.random() * 500);
-  $.getJSON(`https://api.github.com/users?since=${randomOffset}`, (data) => {
-    for (let i = 0; i < 3; i += 1) {
-      const person = data[Math.floor(Math.random() * data.length)];
-      $.getJSON(`https://api.github.com/users/${person.login}`, personDetails => users.concat(personDetails));
-    }
-  });
+  const profiles = getProfilesList();
+  for (let i = 0; i < 3; i += 1) {
+    const profile = getProfile(profiles);
+    users.push(profile);
+  }
   return users;
 }
 
@@ -45,62 +55,3 @@ storeProfiles.subscribe(() => {
 });
 
 refreshButton.click(() => storeProfiles.dispatch({ type: 'REFRESH' }));
-
-// const refreshButton = $('.widget__refresh');
-// const closeButton1 = $('.delete-button-1');
-// const closeButton2 = $('.delete-button-2');
-// const closeButton3 = $('.delete-button-3');
-
-// const refreshClick$ = Observable.fromEvent(refreshButton, 'click');
-// const close1Click$ = Observable.fromEvent(closeButton1, 'click');
-// const close2Click$ = Observable.fromEvent(closeButton2, 'click');
-// const close3Click$ = Observable.fromEvent(closeButton3, 'click');
-
-// const request$ = refreshClick$.startWith('startup click').map(() => {
-//   const randomOffset = Math.floor(Math.random() * 500);
-//   return `https://api.github.com/users?since=${randomOffset}`;
-// });
-
-// const response$ = request$.mergeMap(requestURL =>
-//   Observable.from($.getJSON(requestURL)));
-
-// const createSuggestion$ = closeClick$ =>
-//   closeClick$
-//     .startWith('startup click')
-//     .combineLatest(
-//       response$,
-//       (click, listUsers) =>
-//         listUsers[Math.floor(Math.random() * listUsers.length)],
-//     );
-
-// const createSuggestionDetails$ = suggestion$ =>
-//   suggestion$.mergeMap(suggestedUser =>
-//     Observable.from($.getJSON(`https://api.github.com/users/${suggestedUser.login}`)));
-
-// const suggestion1$ = createSuggestion$(close1Click$);
-// const suggestion2$ = createSuggestion$(close2Click$);
-// const suggestion3$ = createSuggestion$(close3Click$);
-
-// const renderSuggestion = (suggestedUser, selector) => {
-//   const person = $(selector);
-//   person.css('visibility', 'visible');
-//   const name = $(`${selector} .person__name`);
-//   const avatar = $(`${selector} .avatar__image`);
-//   const location = $(`${selector} .location__text`);
-//   const link = $(`${selector} .person__link`);
-//   name.html(suggestedUser.name);
-//   location.html(suggestedUser.location);
-//   link.html(`@${suggestedUser.login}`);
-//   link.attr('href', suggestedUser.html_url);
-//   avatar.css({
-//     background: `url(${suggestedUser.avatar_url}) no-repeat`,
-//     'background-size': 'contain',
-//   });
-// };
-
-// createSuggestionDetails$(suggestion1$).subscribe(suggestedUser =>
-//   renderSuggestion(suggestedUser, '.suggestion-1'));
-// createSuggestionDetails$(suggestion2$).subscribe(suggestedUser =>
-//   renderSuggestion(suggestedUser, '.suggestion-2'));
-// createSuggestionDetails$(suggestion3$).subscribe(suggestedUser =>
-//   renderSuggestion(suggestedUser, '.suggestion-3'));
