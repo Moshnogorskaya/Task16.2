410a564cdd87dc39f926d5bd79bcf62146e8bc08
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-05-29 11:40:30 +0300

    stable clean version

--- src/profiles.js
+++ src/profiles.js
@@ -116,49 +116,52 @@ const initialState = [{
 //   }
 // }
 
-const profileRequest = $.getJSON('https://api.github.com/users/mojombo');
-let profile;
-profileRequest.done((response) => {
-  profile = response;
-});
-console.log(profile);
+async function getProfile() {
+  try {
+    const requestProfile = $.getJSON('https://api.github.com/users/mojombo');
+    const profile = await requestProfile;
+    console.log('inside', profile);
+    return profile;
+  } catch (err) {
+    return console.log('request text failed', err);
+  }
+}
+console.log('outside', getProfile());
 
 function changeState(st) {
   let changedState = st;
-  changedState = changedState.map(() => {
-    return {
-      login: 'mojombo',
-      id: 1,
-      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-      gravatar_id: '',
-      url: 'https://api.github.com/users/mojombo',
-      html_url: 'https://github.com/mojombo',
-      followers_url: 'https://api.github.com/users/mojombo/followers',
-      following_url: 'https://api.github.com/users/mojombo/following{/other_user}',
-      gists_url: 'https://api.github.com/users/mojombo/gists{/gist_id}',
-      starred_url: 'https://api.github.com/users/mojombo/starred{/owner}{/repo}',
-      subscriptions_url: 'https://api.github.com/users/mojombo/subscriptions',
-      organizations_url: 'https://api.github.com/users/mojombo/orgs',
-      repos_url: 'https://api.github.com/users/mojombo/repos',
-      events_url: 'https://api.github.com/users/mojombo/events{/privacy}',
-      received_events_url: 'https://api.github.com/users/mojombo/received_events',
-      type: 'User',
-      site_admin: false,
-      name: 'Tom Preston-Werner',
-      company: null,
-      blog: 'http://tom.preston-werner.com',
-      location: 'San Francisco',
-      email: null,
-      hireable: null,
-      bio: null,
-      public_repos: 60,
-      public_gists: 62,
-      followers: 20965,
-      following: 11,
-      created_at: '2007-10-20T05:24:19Z',
-      updated_at: '2018-05-07T17:19:34Z',
-    };
-  });
+  changedState = changedState.map(() => ({
+    login: 'mojombo',
+    id: 1,
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/mojombo',
+    html_url: 'https://github.com/mojombo',
+    followers_url: 'https://api.github.com/users/mojombo/followers',
+    following_url: 'https://api.github.com/users/mojombo/following{/other_user}',
+    gists_url: 'https://api.github.com/users/mojombo/gists{/gist_id}',
+    starred_url: 'https://api.github.com/users/mojombo/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/mojombo/subscriptions',
+    organizations_url: 'https://api.github.com/users/mojombo/orgs',
+    repos_url: 'https://api.github.com/users/mojombo/repos',
+    events_url: 'https://api.github.com/users/mojombo/events{/privacy}',
+    received_events_url: 'https://api.github.com/users/mojombo/received_events',
+    type: 'User',
+    site_admin: false,
+    name: 'Tom Preston-Werner',
+    company: null,
+    blog: 'http://tom.preston-werner.com',
+    location: 'San Francisco',
+    email: null,
+    hireable: null,
+    bio: null,
+    public_repos: 60,
+    public_gists: 62,
+    followers: 20965,
+    following: 11,
+    created_at: '2007-10-20T05:24:19Z',
+    updated_at: '2018-05-07T17:19:34Z',
+  }));
   return changedState;
 }
 
